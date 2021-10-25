c445c87d3404b4942125a4b7a8f53159d7f069d4
charlie1099 <cdcharlesworth@gmail.com>
2021-10-24 21:05:00 -0600

    added questions and roles

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // node mods
+
 const fs = require('fs');
 const inquirer = require('inquirer');
 
@@ -15,8 +16,78 @@ let employeesArray = [];
 
 // start questions
 
+const managerQuestions = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of the Manager?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log("Please enter manager name?")
+                    return false
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the manager's ID?",
+            validate: idInput => {
+                if (idInput) {
+                    return true
+                } else {
+                    console.log("Please enter a ID for the manager!")
+                    return false
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the manager's email?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true
+                } else {
+                    console.log("Please enter a email for the manager!")
+                    return false
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Please enter the office number the the manager can be reached at!",
+            validate: officeInput => {
+                if (officeInput) {
+                    return true
+                } else {
+                    console.log("Enter a number for the manager!")
+                    return false
+                }
+            }
+        }
+
+
+    ])
+
+    .then(managerInput => {
+        const { name, id, email, officeNumber} = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
+
+        employeesArray.push(manager)
+        console.log(manager)
+    })
+};
+
+
+
+// employee questions
 const newEmployee = () => {
-    return inquirer.prompt([
+    inquirer.prompt([
         {
             type: "input",
             name: "name",
@@ -34,7 +105,7 @@ const newEmployee = () => {
            type: "list",
            name: "role",
            message: "What position will the Emplyee have in the company?", 
-           choices: ["Engineer", "Intern", "Manager"]
+           choices: ["Engineer", "Intern",]
         },
         {
            type: "input",
@@ -61,8 +132,89 @@ const newEmployee = () => {
                 }
             }
         },
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter the Employees githud username.",
+            when: (input) => input.role === "Engineer",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a github username")
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Please enter the intern's school",
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the intern's school!")
+                }
+            }
+        },
+        
+        {
+            type: "confirm",
+            name: "confirmEmployee",
+            message: "Would you like to add more Employee's",
+            default: false
+        }
     ])
-}
+    .then(employeeInput => {
+        let {confirmAddEmployee, school, github, role, name, id, email} = employeeInput
+
+        let employee
+
+        if ( role === "Intern") {
+            employee = new Intern (name, id, email, school)
+
+            console.log(employee)
+        } else if ( role === "Engineer") {
+            employee = new Engineer (github, name, id, email)
+
+            console.log(employee)
+        }
+
+        employeesArray.push(employee)
+
+        if (confirmAddEmployee) {
+            return newEmployee (employeesArray)
+        } else {
+            return employeesArray
+        }
+    })
+
+};
+
+// fs function to write in HTML
+const writeFile = data => {
+    fs.writeFile("../src/index.html", data, err => {
+        if (err) {
+            console.log(err)
+            return;
+        } else {
+            console.log("Success the team profiles is made")
+        }
+    })
+};
+
+managerQuestions()
+.then(employeeQuestions)
+.then(teamArray => {
+    return generateHTML(teamArray)
+})
+.then(pageHTML => {
+    return writeFile(pageHTML)
+})
+.catch(err => {
+    console.log(err)
+})
 
 
 
